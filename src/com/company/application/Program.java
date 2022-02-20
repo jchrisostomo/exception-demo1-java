a72fc871d9e5c90d2452d6f96cdb75dc67cf7b6d
João Chrisóstomo <joao.chrisostomo@gmail.com>
2022-02-20 18:14:08 -0300

    bad solution

--- src/com/company/application/Program.java
+++ src/com/company/application/Program.java
@@ -18,11 +18,11 @@ public class Program {
         System.out.print("Check-in date (dd/MM/yyyy): ");
         Date checkIn = sdf.parse(sc.next());
         System.out.print("Check-out date (dd/MM/yyyy): ");
-        Date chekOut = sdf.parse(sc.next());
+        Date checkOut = sdf.parse(sc.next());
 
 
-        if (chekOut.after(checkIn)) {
-            Reservation reservation = new Reservation(roomNumber, checkIn, chekOut);
+        if (checkOut.after(checkIn)) {
+            Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
             System.out.println(reservation);
 
             System.out.println("----------------------------------------");
@@ -31,33 +31,19 @@ public class Program {
             System.out.print("Check-in date (dd/MM/yyyy): ");
             checkIn = sdf.parse(sc.next());
             System.out.print("Check-out date (dd/MM/yyyy): ");
-            chekOut = sdf.parse(sc.next());
+            checkOut = sdf.parse(sc.next());
 
-            Date now = new Date();
-            if (checkIn.before(now) || chekOut.before(now)) {
-                System.out.println("Error in reservation: Reservation dates for update must be future dates");
-            } else if (chekOut.after(checkIn)) {
-                reservation.updateDates(checkIn, chekOut);
-                System.out.println(reservation);
-            } else {
-                System.out.println("Error in reservation: Check-out date must be after check-in date");
+            String error = reservation.updateDates(checkIn,checkOut);
+            if (error != null) {
+                System.out.println("Error in reservation: " + error);
             }
-        } else {
+            else {
+                System.out.println("Reservation: " + reservation);
+            }
+        }
+        else {
             System.out.println("Error in reservation: Check-out date must be after check-in date");
         }
-
-
-
-/*
-        Room number: 8021
-        Check-in date (dd/MM/yyyy): 23/09/2019
-        Check-out date (dd/MM/yyyy): 26/09/2019
-        Reservation: Room 8021, check-in: 23/09/2019, check-out: 26/09/2019, 3 nights
-        Enter data to update the reservation:
-        Check-in date (dd/MM/yyyy): 24/09/2015
-        Check-out date (dd/MM/yyyy): 29/09/2015
-        Error in reservation: Reservation dates for update must be future dates
-*/
 
         sc.close();
     }
